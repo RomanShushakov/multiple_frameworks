62525d16e87ccdeb4bc1f3ddca234eeb2817fbd0
0jimah <godfreyakuba@gmail.com>
2024-04-15 22:07:57 +0200

    update react app

--- frontend/react/App.tsx
+++ frontend/react/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Home from './components/Home';
@@ -14,7 +13,6 @@ function App() {
     <ToastContainer theme='colored' position='top-center'></ToastContainer>
       <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login/>}></Route>
         <Route path='*' element={<Home/>}></Route>
       </Routes>
       
