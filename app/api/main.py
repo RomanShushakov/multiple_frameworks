ac66bfc25ac115eb7119fdb69c62d99546935930
roman <roman.a.shushakov1@gmail.com>
2024-10-31 14:38:59 +0100

    added pin-check page

--- app/api/main.py
+++ app/api/main.py
@@ -2,10 +2,11 @@
 from fastapi import APIRouter
 
 # internal imports
-from api.routes import items, web_components, translations
+from api.routes import items, web_components, translations, pin_check
 
 
 api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(web_components.router, prefix="/web-components", tags=["web-components"])
 api_router.include_router(translations.router, prefix="/translations", tags=["translations"])
+api_router.include_router(pin_check.router, prefix="/pin-check", tags=["pin-check"])
