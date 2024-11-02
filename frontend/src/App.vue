28fb1184351b3ec7ec2b2847f0f740d18ac281e6
roman shushakov <roman.a.shushakov1@gmail.com>
2024-11-02 15:59:38 +0100

    added users list component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,7 @@ import { ref } from 'vue';
 // internal imports
 import TopBar from './components/TopBar.vue';
 import PinCheckForm from './components/PinCheckForm.vue';
+import UsersList from './components/UsersList.vue';
 
 const usersList = ref([]);
 
@@ -13,28 +14,18 @@ const onUpdateUsersList = (users) => {
 };
 
 const onUsersListError = () => {
+  usersList.value = [];
   alert("ERROR!!!");
 };
 </script>
 
 <template>
-  <!-- <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div> -->
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
   <TopBar />
   <PinCheckForm 
     @update-users-list="onUpdateUsersList($event.users)"
     @users-list-response-error="onUsersListError"
   />
-  <div v-for="user in usersList">
-    {{ user }}
-  </div>
+  <UsersList v-bind={users:usersList} />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/UsersList.vue
@@ -0,0 +1,47 @@
+<script setup>
+// external imports
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+defineProps({
+  users: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class=wrapper>
+    <div class="users-list-container" v-if="users && users.length !== 0">
+      <div name="users-label" class="user-name-label-container">{{ t('usersList') }}</div>
+      <div name="users" v-for="user in users">{{ user }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  margin: 10px 0 0 0;
+  width: 100%;
+  align-items: center;
+}
+
+.users-list-container {
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  align-items: center;
+}
+
+.user-name-label-container {
+  font-size: 16px;
+  font-weight: 600;
+}
+</style>
